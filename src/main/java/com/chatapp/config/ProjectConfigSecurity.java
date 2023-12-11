d4252b9bafddf0d7f6760c16638b3682ea6d4b25
Qophumlando <inkunzikazikamenziephuzeumlaza@gmail.com>
2023-12-11 17:04:41 +0200

    implementing spring default security

--- src/main/java/com/chatapp/config/ProjectConfigSecurity.java
+++ src/main/java/com/chatapp/config/ProjectConfigSecurity.java
@@ -1,4 +1,25 @@
 package com.chatapp.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class ProjectConfigSecurity {
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception
+    {
+        http.csrf(auth->
+                auth.ignoringRequestMatchers("/getSignUpPage")
+                        .ignoringRequestMatchers("/createUser"))
+                .authorizeHttpRequests(request->
+                        request.requestMatchers("","/","/home").permitAll().
+                        requestMatchers("/getChatPage").authenticated())
+                .formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+
 }
