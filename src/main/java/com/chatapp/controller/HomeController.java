e50addec06c2a5b63b1e421029301d5a6a1f29fe
Qophumlando <inkunzikazikamenziephuzeumlaza@gmail.com>
2023-12-11 09:14:38 +0200

    sec commi

--- src/main/java/com/chatapp/controller/HomeController.java
+++ src/main/java/com/chatapp/controller/HomeController.java
@@ -1,7 +1,9 @@
 package com.chatapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -10,5 +12,11 @@ public class HomeController {
     public String findHomePage()
     {
         return "home.html";
+    }
+
+    @GetMapping("/getSignUpPage")
+    public ModelAndView getSignUpPage()
+    {
+        return new ModelAndView("signup");
     }
 }
